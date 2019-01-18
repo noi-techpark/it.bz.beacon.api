8420b41b541e7143cbbbc246e76c8ddf90bf9b7f
Aaron Falk <aaron.falk@raiffeisen.net>
2019-01-18 07:51:54 +0100

    .gitignore fix

--- src/main/java/it/bz/beacon/api/config/BeaconSuedtirolConfiguration.java
+++ src/main/java/it/bz/beacon/api/config/BeaconSuedtirolConfiguration.java
@@ -2,12 +2,10 @@ package it.bz.beacon.api.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 import javax.validation.constraints.NotBlank;
 
 @Configuration
-@PropertySource("classpath:beaconsuedtirol.properties")
 @ConfigurationProperties(prefix = "it.bz.beacon")
 public class BeaconSuedtirolConfiguration {
 
