25aecc481050533bb604bcb11994c2fb5c3f852d
Piiit <pitiz29a@gmail.com>
2019-02-15 15:34:05 +0100

    Update dbinit.sql

--- src/main/resources/dbinit.sql
+++ src/main/resources/dbinit.sql
@@ -8,7 +8,7 @@ create table "user" (
         surname varchar(255),
         username varchar(255),
         primary key (id)
-    )
+    );
 create table beacon_data (
        id int8 not null,
         created_at timestamp not null,
@@ -22,7 +22,7 @@ create table beacon_data (
         manufacturer_id varchar(255) not null,
         name varchar(255),
         primary key (id)
-    )
+    );
 create table beacon_image (
        id int8 not null,
         created_at timestamp not null,
@@ -30,7 +30,7 @@ create table beacon_image (
         beacon_id int8 not null,
         file_name varchar(255) not null,
         primary key (id)
-    )
+    );
 create table issue (
        id int8 not null,
         created_at timestamp not null,
@@ -41,7 +41,7 @@ create table issue (
         reporter varchar(255),
         solution_id int8,
         primary key (id)
-    )
+    );
 create table issue_solution (
        id int8 not null,
         created_at timestamp not null,
@@ -50,11 +50,12 @@ create table issue_solution (
         solution varchar(255) not null,
         solution_description text,
         primary key (id)
-    )
-alter table "user" drop constraint UK_sb8bbouer5wak8vyiiy4pf2bx
-alter table "user" add constraint UK_sb8bbouer5wak8vyiiy4pf2bx unique (username)
-alter table beacon_data drop constraint UK2x2vcndj2x3u5bje9n1sv7cqk
-alter table beacon_data add constraint UK2x2vcndj2x3u5bje9n1sv7cqk unique (manufacturer_id, manufacturer)
-create sequence hibernate_sequence start 1 increment 1
-alter table issue add constraint FKhbve5ev5htnpoykq9lporie1b foreign key (solution_id) references issue_solution
-insert into "user" (id, username, password, name, created_at, updated_at) values (1, 'admin', '$2y$04$CrEwSZC.F21.YuXUN.tSgeMN4ToqQ4Rqb2uzmOqv2R4aUMRuAwHBS', 'Admin', '2019-01-01 00:00:00', '2019-01-01 00:00:00')
+    );
+alter table "user" drop constraint UK_sb8bbouer5wak8vyiiy4pf2bx;
+alter table "user" add constraint UK_sb8bbouer5wak8vyiiy4pf2bx unique (username);
+alter table beacon_data drop constraint UK2x2vcndj2x3u5bje9n1sv7cqk;
+alter table beacon_data add constraint UK2x2vcndj2x3u5bje9n1sv7cqk unique (manufacturer_id, manufacturer);
+create sequence hibernate_sequence start 1 increment 1;
+alter table issue add constraint FKhbve5ev5htnpoykq9lporie1b foreign key (solution_id) references issue_solution;
+
+insert into "user" (id, username, password, name, created_at, updated_at) values (1, 'admin', '$2y$04$CrEwSZC.F21.YuXUN.tSgeMN4ToqQ4Rqb2uzmOqv2R4aUMRuAwHBS', 'Admin', '2019-01-01 00:00:00', '2019-01-01 00:00:00');
