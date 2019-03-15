4baed0ad909774037b21ccdcbed1b126ed7d944e
Aaron Falk <aaron.falk@raiffeisen.net>
2019-03-15 16:10:58 +0100

    transactional usage for infos

--- src/main/java/it/bz/beacon/api/service/info/InfoService.java
+++ src/main/java/it/bz/beacon/api/service/info/InfoService.java
@@ -6,6 +6,7 @@ import it.bz.beacon.api.db.repository.InfoRepository;
 import it.bz.beacon.api.exception.db.InfoNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,43 +20,26 @@ public class InfoService implements IInfoService {
     private BeaconSuedtirolConfiguration beaconSuedtirolConfiguration;
 
     @Override
+    @Transactional
     public List<Info> findAll() {
         return repository.findAll();
     }
 
     @Override
+    @Transactional
     public Info findByBeaconId(String beaconId) throws InfoNotFoundException {
         return repository.findById(beaconId).orElseThrow(InfoNotFoundException::new);
     }
 
     @Override
+    @Transactional
     public Info findByInstanceId(String instanceId) throws InfoNotFoundException {
         return repository.findByNamespaceAndInstanceId(beaconSuedtirolConfiguration.getNamespace(), instanceId).orElseThrow(InfoNotFoundException::new);
     }
 
     @Override
+    @Transactional
     public Info findByMajorMinor(int major, int minor) throws InfoNotFoundException {
         return repository.findByUuidAndMajorAndMinor(beaconSuedtirolConfiguration.getUuid(), major, minor).orElseThrow(InfoNotFoundException::new);
     }
-
-//    @Override
-//    public Info create(InfoCreation infoCreation) {
-//        try {
-//            return repository.save(Info.create(infoCreation, beaconSuedtirolConfiguration.getUuid(), beaconSuedtirolConfiguration.getNamespace()));
-//        } catch (DataIntegrityViolationException e) {
-//            throw new DuplicateEntryException();
-//        }
-//    }
-//
-//    @Override
-//    public Info update(long id, InfoUpdate infoUpdate) throws UserNotFoundException {
-//        return repository.findById(id).map(info -> {
-//            if (infoUpdate.getPassword() != null) {
-//
-//            }
-////            info.applyUpdate(infoUpdate);
-////
-//            return repository.save(info);
-//        }).orElseThrow(UserNotFoundException::new);
-//    }
 }
