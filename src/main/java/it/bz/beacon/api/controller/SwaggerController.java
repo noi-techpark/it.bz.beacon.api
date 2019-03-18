7136e98b7cf1fe1ef3ce15f10b3df88876b75e9c
Aaron Falk <aaron.falk@raiffeisen.net>
2019-03-18 09:46:17 +0100

    beacon list fix for empty beacondatalist

--- src/main/java/it/bz/beacon/api/controller/SwaggerController.java
+++ src/main/java/it/bz/beacon/api/controller/SwaggerController.java
@@ -2,8 +2,10 @@ package it.bz.beacon.api.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springfox.documentation.annotations.ApiIgnore;
 
-@Controller //note - this is a spring-boot controller, not @RestController
+@ApiIgnore
+@Controller
 public class SwaggerController {
 
     @RequestMapping("/")
