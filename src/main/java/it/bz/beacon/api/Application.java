e0a8d5d6dfc10a7e333daab21f986198b017e7ee
Alex Lanz <alex@krumer.it>
2019-03-26 09:03:14 +0100

    Testing base path of swagger

--- src/main/java/it/bz/beacon/api/Application.java
+++ src/main/java/it/bz/beacon/api/Application.java
@@ -30,6 +30,7 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.paths.RelativePathProvider;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -66,6 +67,12 @@ public class Application extends SpringBootServletInitializer {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .host(apiInfoConfiguration.getHost())
+                .pathProvider(new RelativePathProvider(servletContext) {
+                    @Override
+                    public String getApplicationBasePath() {
+                        return "/";
+                    }
+                });
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("it.bz.beacon.api.controller"))
                 .build()
