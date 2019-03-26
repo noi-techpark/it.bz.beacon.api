af904077e18d591b7a8b258998d298b841b43319
Alex Lanz <alex@krumer.it>
2019-03-26 14:04:31 +0100

    Reverting swagger config

--- src/main/java/it/bz/beacon/api/Application.java
+++ src/main/java/it/bz/beacon/api/Application.java
@@ -5,7 +5,6 @@ import com.google.common.collect.Lists;
 import it.bz.beacon.api.config.ApiInfoConfiguration;
 import it.bz.beacon.api.config.BeaconSuedtirolConfiguration;
 import it.bz.beacon.api.config.KontaktIOConfiguration;
-import javax.servlet.ServletContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -31,7 +30,6 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
-import springfox.documentation.spring.web.paths.RelativePathProvider;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -60,9 +58,6 @@ public class Application extends SpringBootServletInitializer {
     @Autowired
     private ApiInfoConfiguration apiInfoConfiguration;
 
-    @Autowired
-    ServletContext servletContext;
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
@@ -71,12 +66,6 @@ public class Application extends SpringBootServletInitializer {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .host(apiInfoConfiguration.getHost())
-                .pathProvider(new RelativePathProvider(servletContext) {
-                    @Override
-                    public String getApplicationBasePath() {
-                        return "";
-                    }
-                })
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("it.bz.beacon.api.controller"))
                 .build()
