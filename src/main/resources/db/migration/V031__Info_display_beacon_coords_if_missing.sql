69f08d082daee606a35c8ae2d56a75c104d81cf4
Thomas Nocker <t.nocker.tn@gmail.com>
2020-10-05 19:55:37 +0200

    Info: show beacon coordinates if poi coordinates are null or 0

---
+++ src/main/resources/db/migration/V031__Info_display_beacon_coords_if_missing.sql
@@ -0,0 +1,23 @@
+create or replace view info as
+select id as id,
+       created_at,
+       updated_at,
+       address,
+       cap,
+       case when lat_poi is not null and lat_poi != 0 then lat_poi else lat_beacon end as latitude,
+       location,
+       case when lng_poi is not null and lng_poi != 0 then lng_poi else lng_beacon end as longitude,
+       floor,
+       instance_id,
+       major,
+       minor,
+       name_poi as name,
+       namespace,
+       uuid,
+       website,
+       battery_level,
+       trusted_updated_at,
+       ( select count(*)::integer as count
+         from issue
+         where issue.beacon_data_id::text = beacon_data.id::text and issue.solution_id is null) as open_issue_count
+from beacon_data;
