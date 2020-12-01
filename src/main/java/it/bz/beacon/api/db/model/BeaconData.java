ffec62c0f68d25bbb9061c5b3c64a1985c933ce9
Thomas Nocker <t.nocker.tn@gmail.com>
2020-12-01 10:21:54 +0100

    assign all unassigned beacons to 'Unassigned' group. beacon group set to not null

--- src/main/java/it/bz/beacon/api/db/model/BeaconData.java
+++ src/main/java/it/bz/beacon/api/db/model/BeaconData.java
@@ -57,7 +57,7 @@ public class BeaconData extends AuditModel {
     private List<Issue> issues = new ArrayList<>();
 
     @ManyToOne
-    @JoinColumn(name = "group_id", nullable = true, updatable = true)
+    @JoinColumn(name = "group_id", nullable = false, updatable = true)
     private Group group;
 
     @Column(name = "trusted_updated_at")
