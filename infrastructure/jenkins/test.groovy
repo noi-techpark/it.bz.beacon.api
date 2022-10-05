e77993fde85be934ff9d3356b0da0df2c9e9a8cc
Simon Dalvai <s.dalvai@noi.bz.it>
2022-10-05 11:13:03 +0200

    jenkins: fix docker file name

--- infrastructure/jenkins/test.groovy
+++ infrastructure/jenkins/test.groovy
@@ -1,7 +1,7 @@
 pipeline {
     agent {
         dockerfile {
-            filename 'infrastructure/docker/java.dockerfile'
+            filename 'infrastructure/docker/Dockerfile'
             additionalBuildArgs '--build-arg JENKINS_USER_ID=$(id -u jenkins) --build-arg JENKINS_GROUP_ID=$(id -g jenkins)'
         }
     }
