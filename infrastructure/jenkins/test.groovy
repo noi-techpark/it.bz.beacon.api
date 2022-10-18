221a789a7630afebff74eeef56d26afedadb64e6
Simon Dalvai <s.dalvai@noi.bz.it>
2022-10-18 16:45:31 +0200

    revert to pre Github Actions

--- infrastructure/jenkins/test.groovy
+++ infrastructure/jenkins/test.groovy
@@ -1,7 +1,7 @@
 pipeline {
     agent {
         dockerfile {
-            filename 'infrastructure/docker/Dockerfile'
+            filename 'infrastructure/docker/java.dockerfile'
             additionalBuildArgs '--build-arg JENKINS_USER_ID=$(id -u jenkins) --build-arg JENKINS_GROUP_ID=$(id -g jenkins)'
         }
     }
